09429c2b086dfb85e7c30fd28bd1147ae83bd650
Søren Kyndi-Wiuff <skw@lunar.app>
2020-06-20 12:35:47 +0200

    adds projection support to the optimistic locking stream

--- stream_optimistic_locking.go
+++ stream_optimistic_locking.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"sync"
 
+	"github.com/go-po/po/internal/record"
 	"github.com/go-po/po/streams"
 )
 
@@ -12,7 +13,13 @@ var _ messageStream = &OptimisticLockingStream{}
 type optimisticStore interface {
 	WriteRecord(ctx context.Context, id streams.Id, contentType string, b []byte) (int64, error)
 	WriteRecordAt(ctx context.Context, id streams.Id, contentType string, b []byte, position int64) error
+
+	ReadRecords(ctx context.Context, id streams.Id, from int64) ([]record.Record, error)
+	snapshotStore
 }
+
+type appenderFunc func(ctx context.Context, id streams.Id, position int64, message interface{}) (int64, error)
+type projectorFunc func(ctx context.Context, id streams.Id, lockPosition int64, projection Handler) (int64, error)
 
 func NewOptimisticLockingStream(ctx context.Context, streamId streams.Id, store optimisticStore, registry Registry) *OptimisticLockingStream {
 	return &OptimisticLockingStream{
@@ -20,18 +27,26 @@ func NewOptimisticLockingStream(ctx context.Context, streamId streams.Id, store 
 		ctx:          ctx,
 		store:        store,
 		registry:     registry,
+		projector:    newProjectorFunc(store, newSnapshots(store), registry),
+		appender:     newAppenderFunc(store, registry),
 		mu:           sync.RWMutex{},
 		lockPosition: -1,
 	}
 }
 
+type Snapshots interface {
+	Snapshot(ctx context.Context, id streams.Id, projection Handler) (snapshotCommit, int64, error)
+}
+
 // Stream that uses Optimistic locking when
 // appending to the message stream
 type OptimisticLockingStream struct {
-	Id       streams.Id
-	ctx      context.Context // to use for the operation
-	store    optimisticStore
-	registry Registry
+	Id        streams.Id
+	ctx       context.Context // to use for the operation
+	store     optimisticStore
+	registry  Registry
+	projector projectorFunc
+	appender  appenderFunc
 
 	mu           sync.RWMutex // protects the fields below
 	lockPosition int64        // last known position
@@ -43,7 +58,7 @@ func (stream *OptimisticLockingStream) Append(messages ...interface{}) (int64, e
 
 	var err error
 	for _, msg := range messages {
-		stream.lockPosition, err = stream.append(stream.lockPosition, msg)
+		stream.lockPosition, err = stream.appender(stream.ctx, stream.Id, stream.lockPosition, msg)
 		if err != nil {
 			return stream.lockPosition, err
 		}
@@ -51,29 +66,103 @@ func (stream *OptimisticLockingStream) Append(messages ...interface{}) (int64, e
 	return stream.lockPosition, nil
 }
 
-func (stream *OptimisticLockingStream) append(position int64, message interface{}) (int64, error) {
-	b, contentType, err := stream.registry.Marshal(message)
-	if err != nil {
-		return -1, err
-	}
-
-	if position < 0 {
-		// this append have not seen the lockPosition yet,
-		// so have to get it from the store when performing the first write
-		return stream.store.WriteRecord(stream.ctx, stream.Id, contentType, b)
-	}
-
-	err = stream.store.WriteRecordAt(stream.ctx, stream.Id, contentType, b, position+1)
-	if err != nil {
-		return -1, err
-	}
-	return position + 1, nil
-}
-
+// Projects all messages onto the given Handler.
+// If the handler implements streams.NamedSnapshot, snapshotting will be performed.
+//
+// The projection will also lock this Stream instance to the most recently read
+// message number for the stream.
 func (stream *OptimisticLockingStream) Project(projection Handler) error {
-	panic("implement me")
+	// TODO add observability
+	stream.mu.Lock()
+	defer stream.mu.Unlock()
+
+	position, err := stream.projector(stream.ctx, stream.Id, stream.lockPosition, projection)
+	if err != nil {
+		return err
+	}
+	stream.lockPosition = position
+	return nil
 }
 
 func (stream *OptimisticLockingStream) Execute(exec CommandHandler) error {
 	panic("implement me")
+}
+
+func newProjectorFunc(store optimisticStore, snap Snapshots, registry Registry) projectorFunc {
+	return func(ctx context.Context, id streams.Id, lockPosition int64, projection Handler) (int64, error) {
+		snapCommit, snapPosition, err := snap.Snapshot(ctx, id, projection)
+		if err != nil {
+			return -1, err
+		}
+		if snapPosition > -1 {
+			lockPosition = snapPosition
+		}
+
+		records, err := store.ReadRecords(ctx, id, lockPosition)
+		if err != nil {
+			return -1, err
+		}
+
+		if len(records) == 0 {
+			// nothing new, bail out
+			return lockPosition, nil
+		}
+
+		var messages []streams.Message
+		for _, r := range records {
+			message, err := registry.ToMessage(r)
+			if err != nil {
+				return -1, err
+			}
+			messages = append(messages, message)
+		}
+
+		for _, message := range messages {
+			err = projection.Handle(ctx, message)
+			if err != nil {
+				return -1, err
+			}
+		}
+
+		// store the position as the stream object is now considered active
+		// This to guarantee users of the projection that messages appended
+		// afterwards will be in the order their projection was made.
+		if len(messages) == 0 {
+			return lockPosition, nil
+		}
+		message := messages[len(messages)-1]
+		if id.HasEntity() {
+			lockPosition = message.Number
+		} else {
+			lockPosition = message.GroupNumber
+		}
+
+		err = snapCommit(lockPosition)
+		if err != nil {
+			return lockPosition, err
+		}
+
+		return lockPosition, nil
+	}
+}
+
+func newAppenderFunc(store optimisticStore, registry Registry) appenderFunc {
+	return func(ctx context.Context, id streams.Id, position int64, message interface{}) (int64, error) {
+		b, contentType, err := registry.Marshal(message)
+		if err != nil {
+			return -1, err
+		}
+
+		if position < 0 {
+			// this append have not seen the lockPosition yet,
+			// so have to get it from the store when performing the first write
+			return store.WriteRecord(ctx, id, contentType, b)
+		}
+
+		err = store.WriteRecordAt(ctx, id, contentType, b, position+1)
+		if err != nil {
+			return -1, err
+		}
+		return position + 1, nil
+	}
 }
--- stream_optimistic_locking_test.go
+++ stream_optimistic_locking_test.go
@@ -3,22 +3,70 @@ package po
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/go-po/po/internal/record"
 	"github.com/go-po/po/internal/registry"
 	"github.com/go-po/po/internal/store"
 	"github.com/go-po/po/streams"
 	"github.com/stretchr/testify/assert"
 )
 
+type Msg struct {
+	Name string
+}
+
+var testRegistry = registry.New()
+
+func init() {
+	testRegistry.Register(func(b []byte) (interface{}, error) {
+		msg := Msg{}
+		err := json.Unmarshal(b, &msg)
+		return msg, err
+	})
+}
+
 var _ optimisticStore = &stubStore{}
 
 type stubStore struct {
 	messageCount int64
+	records      []record.Record
+	snapshot     record.Snapshot
+}
+
+func (stub *stubStore) ReadRecords(ctx context.Context, id streams.Id, from int64) ([]record.Record, error) {
+	if from < 0 {
+		return stub.records, nil
+	}
+	return stub.records[from:], nil
+}
+
+func (stub *stubStore) ReadSnapshot(ctx context.Context, id streams.Id, snapshotId string) (record.Snapshot, error) {
+	return stub.snapshot, nil
+}
+
+func (stub *stubStore) UpdateSnapshot(ctx context.Context, id streams.Id, snapshotId string, snapshot record.Snapshot) error {
+	stub.snapshot = snapshot
+	return nil
+}
+
+func (stub *stubStore) incCount() {
+	stub.messageCount = stub.messageCount + 1
 }
 
 func (stub *stubStore) WriteRecord(ctx context.Context, id streams.Id, contentType string, b []byte) (int64, error) {
-	stub.messageCount = stub.messageCount + 1
+	stub.incCount()
+	stub.records = append(stub.records, record.Record{
+		Stream:      id,
+		Number:      stub.messageCount,
+		GroupNumber: stub.messageCount,
+		Data:        b,
+		Group:       id.Group,
+		ContentType: contentType,
+		Time:        time.Now(),
+	})
 	return stub.messageCount, nil
 }
 
@@ -29,22 +77,135 @@ func (stub *stubStore) WriteRecordAt(ctx context.Context, id streams.Id, content
 			Position: position,
 		}
 	}
-	stub.messageCount = stub.messageCount + 1
+	stub.incCount()
+	stub.records = append(stub.records, record.Record{
+		Stream:      id,
+		Number:      stub.messageCount,
+		GroupNumber: stub.messageCount,
+		Data:        b,
+		Group:       id.Group,
+		ContentType: contentType,
+		Time:        time.Now(),
+	})
 	return nil
+}
+
+type stubSnapshotter struct {
+	position int64
+}
+
+func (stub *stubSnapshotter) Snapshot(ctx context.Context, id streams.Id, projection Handler) (snapshotCommit, int64, error) {
+	return func(position int64) error {
+		stub.position = position
+		return nil
+	}, stub.position, nil
+}
+
+func newProjectionHandler(handler HandlerFunc) *stubProjectionHandler {
+	return &stubProjectionHandler{
+		handler: handler,
+	}
+}
+
+type stubProjectionHandler struct {
+	handler Handler
+}
+
+func (stub *stubProjectionHandler) SnapshotName() string {
+	return "stub-snapshot"
+}
+
+func (stub *stubProjectionHandler) Handle(ctx context.Context, msg streams.Message) error {
+	return stub.handler.Handle(ctx, msg)
+}
+
+func TestOptimisticLockingStream_Project(t *testing.T) {
+	ctx := context.Background()
+	streamId := streams.ParseId("teststream-1")
+
+	// setup
+	newTestFixture := func(messageCount int64, snap *stubSnapshotter) projectorFunc {
+		if snap == nil {
+			snap = &stubSnapshotter{}
+		}
+		store := &stubStore{messageCount: 0}
+		appender := newAppenderFunc(store, testRegistry)
+		var i int64
+		for i = 0; i < messageCount; i++ {
+			_, err := appender(ctx, streamId, i, Msg{Name: fmt.Sprintf("Message %d", i)})
+			assert.NoError(t, err)
+		}
+		return newProjectorFunc(store, snap, testRegistry)
+	}
+
+	t.Run("empty stream", func(t *testing.T) {
+		// setup
+		sut := newTestFixture(0, nil)
+		count := 0
+		// execute
+		pos, err := sut(ctx, streamId, -1, HandlerFunc(func(ctx context.Context, msg streams.Message) error {
+			count = count + 1
+			return nil
+		}))
+		// verify
+		assert.NoError(t, err)
+		assert.Equal(t, 0, count)
+		assert.Equal(t, 0, int(pos))
+	})
+
+	t.Run("with data", func(t *testing.T) {
+		// setup
+		sut := newTestFixture(10, nil)
+		count := 0
+		// execute
+		pos, err := sut(ctx, streamId, -1, HandlerFunc(func(ctx context.Context, msg streams.Message) error {
+			count = count + 1
+			return nil
+		}))
+		// verify
+		assert.NoError(t, err)
+		assert.Equal(t, 10, count, "messages received")
+		assert.Equal(t, 10, int(pos))
+	})
+
+	t.Run("existing", func(t *testing.T) {
+		// setup
+		snap := &stubSnapshotter{}
+		sut := newTestFixture(10, snap)
+		count := 0
+		// execute
+		pos, err := sut(ctx, streamId, -1, HandlerFunc(func(ctx context.Context, msg streams.Message) error {
+			count = count + 1
+			return nil
+		}))
+		// verify
+		assert.NoError(t, err)
+		assert.Equal(t, 10, count, "messages received")
+		assert.Equal(t, 10, int(pos))
+		assert.Equal(t, 10, int(snap.position))
+	})
+
+	t.Run("existing with data", func(t *testing.T) {
+		// setup
+		snap := &stubSnapshotter{position: 4}
+		sut := newTestFixture(10, snap)
+		count := 0
+		// execute
+		pos, err := sut(ctx, streamId, -1, HandlerFunc(func(ctx context.Context, msg streams.Message) error {
+			count = count + 1
+			return nil
+		}))
+		// verify
+		assert.NoError(t, err)
+		assert.Equal(t, 6, count, "messages received")
+		assert.Equal(t, 10, int(pos))
+		assert.Equal(t, 10, int(snap.position))
+	})
 }
 
 func TestOptimisticLockingStream_Append(t *testing.T) {
 	// setup
-	type Msg struct {
-		Name string
-	}
-	testRegistry := registry.New()
-	testRegistry.Register(func(b []byte) (interface{}, error) {
-		msg := Msg{}
-		err := json.Unmarshal(b, &msg)
-		return msg, err
-	})
-	newEntityStream := func(lockPosition int64, messageCount int64) (*OptimisticLockingStream, *stubStore) {
+	newTestFixture := func(lockPosition int64, messageCount int64) (*OptimisticLockingStream, *stubStore) {
 		store := &stubStore{messageCount: messageCount}
 		stream := NewOptimisticLockingStream(
 			context.Background(),
@@ -82,7 +243,7 @@ func TestOptimisticLockingStream_Append(t *testing.T) {
 
 	t.Run("fresh stream", func(t *testing.T) {
 		// setup
-		stream, store := newEntityStream(-1, 0)
+		stream, store := newTestFixture(-1, 0)
 		// execute
 		n, err := stream.Append(Msg{Name: "Append Test"})
 		// verify
@@ -91,7 +252,7 @@ func TestOptimisticLockingStream_Append(t *testing.T) {
 
 	t.Run("existing stream", func(t *testing.T) {
 		// setup
-		stream, store := newEntityStream(-1, 3)
+		stream, store := newTestFixture(-1, 3)
 		// execute
 		n, err := stream.Append(Msg{Name: "Append Test"})
 		// verify
@@ -100,7 +261,7 @@ func TestOptimisticLockingStream_Append(t *testing.T) {
 
 	t.Run("locked stream", func(t *testing.T) {
 		// setup
-		stream, store := newEntityStream(3, 3)
+		stream, store := newTestFixture(3, 3)
 		// execute
 		n, err := stream.Append(Msg{Name: "Append Test"})
 		// verify
@@ -109,7 +270,7 @@ func TestOptimisticLockingStream_Append(t *testing.T) {
 
 	t.Run("locked stream with conflict", func(t *testing.T) {
 		// setup
-		stream, store := newEntityStream(3, 5)
+		stream, store := newTestFixture(3, 5)
 		// execute
 		n, err := stream.Append(Msg{Name: "Append Test"})
 		// verify
