a9c877c3e461c54edaeda820fcbf3f38aa866be5
Søren Kyndi-Wiuff <skw@lunar.app>
2020-08-16 21:24:47 +0200

    Adds reconnect to RabbitMQ Protocol (#37)
    * adds a reconnect mechanism to the rabbit transport
    
    * lowers the log level on the reconnect mechanism

--- internal/broker/rabbitmq/logger.go
+++ internal/broker/rabbitmq/logger.go
@@ -2,10 +2,14 @@ package rabbitmq
 
 type Logger interface {
 	Errorf(template string, args ...interface{})
+	Infof(template string, args ...interface{})
 }
 
 type noopLogger struct{}
 
+func (noopLogger) Infof(template string, args ...interface{}) {
+
+}
 func (noopLogger) Errorf(template string, args ...interface{}) {
 	// noop
 }
